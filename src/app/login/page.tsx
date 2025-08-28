b5a7b926cdfdf02396e3033a2abf9e2d5fb073d9
Adriano549 <165341189+Adriano549@users.noreply.github.com>
2025-08-28 18:31:14 -0300

    fix(deploy): testando para o vercel

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,7 @@
 import LoginForm from '@/components/shared/LoginForm';
 
+export const dynamic = 'force-dynamic'
+
 export default function LoginPage() {
     return (
         <main className="flex min-h-screen items-center justify-center bg-muted/40 p-4">
